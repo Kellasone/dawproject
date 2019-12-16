cf36c93f3a4ae4767284b71966d453260f26cd00
malinabenegui <malina.benegui@gmail.com>
2019-12-16 19:23:22 +0200

    Minor changes

--- MarkIT/Controllers/AccountController.cs
+++ MarkIT/Controllers/AccountController.cs
@@ -86,7 +86,7 @@ namespace MarkIT.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Wrong email or password");
                     return View(model);
             }
         }
