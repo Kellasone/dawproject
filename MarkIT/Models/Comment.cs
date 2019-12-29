28f5ad4dcb5ea38b436cb2503489b808dbd372a5
Alexandru Mihai <a_mihai_lo@yahoo.ro>
2019-12-29 07:53:40 +0200

    added models for votes and comments. also made mihai@admin.com an admin

---
+++ MarkIT/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarkIT.Models
+{
+	public class Comment
+	{
+		[Key]
+		public int CommentId { get; set; }
+		[Required]
+		public string Content { get; set; }
+		[Required]
+		public int BookmarkId { get; set; }
+	}
+}
---
+++ MarkIT/Models/Vote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarkIT.Models
+{
+	public class Vote
+	{
+		[Key]
+		public int VoteId { get; set; }
+		[Required]
+		public string UserId { get; set; }
+		[Required]
+		public int BookmarkId { get; set; }
+	}
+}
