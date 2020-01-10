b139d50fbf5d99662107bfdf64f466217498ec18
Alexandru Mihai <a_mihai_lo@yahoo.ro>
2020-01-10 10:43:35 +0200

    final changes

--- MarkIT/Controllers/CategoryController.cs
+++ MarkIT/Controllers/CategoryController.cs
@@ -7,9 +7,18 @@ using System.Web.Mvc;
 
 namespace MarkIT.Controllers
 {
-    public class CategoryController : Controller
-    {
+	public class CategoryController : Controller
+	{
 		private ApplicationDbContext db = new ApplicationDbContext();
+		[Authorize(Roles = "Administrator")]
+		public ActionResult Show()
+		{
+			var categories = db.Category.ToList();
+			ViewBag.categories = categories;
+			return View();
+		}
+
+
 
 		[Authorize(Roles = "User,Administrator")]
 		public ActionResult New()
@@ -44,7 +53,40 @@ namespace MarkIT.Controllers
 				return View(category);
 			}
 		}
+		[Authorize(Roles = "Administrator")]
+		public ActionResult Delete(int id)
+		{
+			var category = db.Category.Find(id);
+			db.Category.Remove(category);
 
+			var bookmarksToBeDeleted = db.SavedBookmarks.Where(m => m.CategoryId == id);
 
+			foreach (var bookmark in bookmarksToBeDeleted)
+			{
+				db.SavedBookmarks.Remove(bookmark);
+			}
+
+			db.SaveChanges();
+			return RedirectToAction("Show", "Category");
+		}
+
+		[Authorize(Roles = "Administrator")]
+		public ActionResult Edit(int id)
+		{
+			Category category = db.Category.Find(id);
+			ViewBag.Category = category;
+			return View(category);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(int id, Bookmark requestCategory)
+		{
+			Category category = db.Category.Find(id);
+
+			category.Title = requestCategory.Title;
+			db.SaveChanges();
+			return RedirectToAction("Show/", "category");
+
+		}
 	}
 }
